4d029d5a46959fd71c439ac2567c7342423191f5
nareshungarala396 <nareshungarala220@gmail.com>
2026-02-07 15:33:30 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "catalogue" {
 
   ami = var.ami_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = "local.catalogue_sg_id"
+  vpc_security_group_ids = [local.catalogue_sg_id]
   subnet_id = local.private_subnet_id
 
     tags = merge (
