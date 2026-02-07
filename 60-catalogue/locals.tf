7caed36190110fcbde25bbc75c312b0dfd35b8e1
nareshungarala396 <nareshungarala220@gmail.com>
2026-02-07 15:39:06 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,9 +1,9 @@
 locals {
     # ami_id = data.aws_ami.joindevops.id
     common_name_suffix = "${var.project_name}-${var.environment}"
-    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id
-    vpc_id = data.aws_ssm_parameter.vpc_id
-    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
     private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
     common_tags = {
