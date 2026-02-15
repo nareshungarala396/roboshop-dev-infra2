076e78adf38873506dd5a020445fe5b34d0344a1
nareshungarala396 <nareshungarala220@gmail.com>
2026-02-15 12:35:01 +0530

    terraform

--- 80-frontend-alb/parameters.tf
+++ 80-frontend-alb/parameters.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "frontend_alb_listener_arn" {
   name = "/${var.project_name}/${var.environment}/frontend_alb_listener_arn"
   type = "String"
-  value = aws_lb_listener.frontend_alb.arn
+  value = aws_lb_listener.front_end.arn
+
 }
