5360a72c6adcd99c077def75631515f500a6fe05
nareshungarala396 <nareshungarala220@gmail.com>
2026-02-15 14:19:49 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -6,7 +6,7 @@
 
 module "components" {
     for_each = var.components
-    source = "../../terraform-roboshop-component"
+    source = "git::https://github.com/nareshungarala396/terraform-roboshop-component.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
 }
